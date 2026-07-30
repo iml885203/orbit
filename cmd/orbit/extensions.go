49b6c774130ed2e3ea8a8b5c56ad403e2f77e8e4
Logan <iml885203@gmail.com>
2026-07-30 13:09:40 +0800

    chore(release): prepare v0.1.0

--- cmd/orbit/extensions.go
+++ cmd/orbit/extensions.go
@@ -34,7 +34,7 @@ func Extensions() []extension.Extension {
 		},
 		Distribution: &extension.Distribution{
 			EnvRepoURL: "https://github.com/iml885203/orbit-demo.git",
-			EnvRepoRef: "v0.0.46",
+			EnvRepoRef: "v0.1.0",
 			InstallURL: "https://raw.githubusercontent.com/iml885203/orbit/main/scripts/install.sh",
 			DefaultEnv: "quickstart.yaml",
 		},
