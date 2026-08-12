d72a2529bf8017d19da9b9d1b0558b7e5657acf2
Logan <iml885203@gmail.com>
2026-08-12 16:13:54 +0800

    build(release): pin the v0.14.3 demo at its staged -rc revision

--- cmd/orbit/extensions.go
+++ cmd/orbit/extensions.go
@@ -37,7 +37,7 @@ func Extensions() []extension.Extension {
 		},
 		Distribution: &extension.Distribution{
 			EnvRepoURL: "https://github.com/iml885203/orbit-demo.git",
-			EnvRepoRef: "v0.14.3",
+			EnvRepoRef: "v0.14.3-rc",
 			InstallURL: "https://raw.githubusercontent.com/iml885203/orbit/main/scripts/install.sh",
 			DefaultEnv: "quickstart.yaml",
 		},
